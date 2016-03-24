8c0d74501fdcea1cdf9ee4aae8a9a351daeff98b
vietdh85 <hoangviet.vh@gmail.com>
2016-03-24 18:22:44 +0700

    hyip_scope.py

---
+++ script/common.py
@@ -0,0 +1,54 @@
+import sys
+import os.path
+from datetime import datetime
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "lib")))
+import util
+import mysql
+
+def insertSite(obj):
+	query = """
+		SELECT id FROM sites WHERE url = '{0}'
+	""".format(obj['url'])
+	
+	id = mysql.executeScalar(query)
+	
+	if id == None:
+		query = """
+			INSERT INTO sites(url) values ('{0}')
+		""".format(obj['url'])
+		
+		id = mysql.executeInsert(query)
+		
+	return id 
+
+def insertSiteMonitor(obj, monitor):
+	query = """
+		INSERT IGNORE INTO site_monitor(site_id, monitor, ref_site_id, ref_site_url) values ({0}, '{1}', {2}, '{3}')
+	""".format(obj['siteId'], monitor, obj['id'], obj['siteRCBUrl'])
+	
+	return mysql.executeNoneQuery(query)
+
+def insertUnknowSite(url, monitor):
+	query = """
+		INSERT IGNORE INTO unknow_sites(url, monitor) values ('{0}', '{1}')
+	""".format(url, monitor)
+	
+	return mysql.executeNoneQuery(query)
+	
+def setPaid(siteId):
+	query = """
+		UPDATE site_monitor SET is_paid = 1 WHERE site_id = {0}
+	""".format(siteId)
+	
+	return mysql.executeNoneQuery(query)	
+	
+def getSiteMonitor(monitor):
+	query = """
+		SELECT site_id, ref_site_url, is_paid
+		FROM site_monitor sm
+		JOIN sites s ON s.id = sm.site_id AND s.is_scam = 0
+		WHERE sm.is_paid = 0 AND sm.monitor = '{0}'
+	""".format(monitor)
+	
+	return mysql.executeQuery(query)
---
+++ script/hyip_tank.py
@@ -0,0 +1,89 @@
+import sys
+import os.path
+import urllib2
+import re
+from pyquery import PyQuery as pq
+
+import common
+
+def getId(url):
+	arr = url.split("=")
+	id = arr[len(arr) - 1]
+	return id
+
+def getSiteUrl(id, monitor):
+	urlRequest = "http://hyiptank.net/hyip-{0}.html".format(id)
+	result = ""
+	print("REQUEST: {0}".format(urlRequest))
+	
+	try:
+		req = urllib2.urlopen(urlRequest, timeout=30)
+		url = req.geturl()
+	
+		arr = url.split("/?")
+		arr1 = arr[0].split("//")
+		
+		result = arr1[1].replace("www.", "")
+		result = result.split("/")[0]
+	except :
+		print("========== ERROR ===========")
+		common.insertUnknowSite(urlRequest, monitor)
+	
+	return result
+	
+def getRcb(monitor):	
+	print("hyip_tank.getRcb()")
+
+	rcb_url = "http://{0}/allrcb.html".format(monitor)
+	d = pq(url=rcb_url)
+	tables = d("table.listbody tr td[align='right'] a")
+	siteList = []
+	
+	for index, item in enumerate(tables):
+		if index % 2 == 0 and index < len(tables) - 2:
+			obj = {}
+			obj['id'] = getId(item.get("href"))
+			obj['url'] = getSiteUrl(obj['id'], monitor)
+			obj['siteId'] = ""
+			obj['siteRCBUrl'] = ""
+			
+			if obj['url'] != '':
+				siteId = common.insertSite(obj)
+				obj['siteId'] = siteId
+				obj['siteRCBUrl'] = "http://{0}/rcb-{1}.html".format(monitor, obj['id'])
+				
+				print("{0} - {1} - {2}".format(obj['id'], obj['url'], obj['siteId']))
+				
+			siteList.append(obj)
+					
+	for item in siteList:
+		common.insertSiteMonitor(item, monitor)
+
+def checkPaid(siteUrl):
+	d = pq(url=siteUrl)
+	tables = d("#content2 table.listbody tr td:nth-child(6) center")
+	result = False
+	
+	#print(tables)
+	for item in tables:
+		if re.search('paid', item.text_content(), re.IGNORECASE):
+			result = True
+			
+	return result
+
+def checkRcb(monitor):
+	siteMonitors = common.getSiteMonitor(monitor)
+	for item in siteMonitors:
+		print(item)
+		if item[2] == 0:
+			if checkPaid(item[1]):
+				setPaid(item[0])
+		
+def run():
+	MONITOR = "hyiptank.net"
+	getRcb(MONITOR)
+	#checkRcb(MONITOR)
+
+
+
+

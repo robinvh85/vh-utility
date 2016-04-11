197e687fd0bb4cf083b4eb6eb5ecb4f18c0f05b0
vietdh85 <hoangviet.vh@gmail.com>
2016-04-11 17:59:32 +0700

    Script

--- script/stat/get_stat.py
+++ script/stat/get_stat.py
@@ -8,43 +8,25 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../lib"
 import mysql
 import util
 
-import webflipfunds_com
 import custommining_com
 import unitedswissbanks_com
-import nanoproinvest_com
-import bitcoinnetworks_biz
-import city_finance_org
-import greed_rush_com
-import bitcoinpalas_com
 import depdun_com
-import paragoninvest_biz
 import bullair_biz
 import oktrade_biz
 import lucrativeventure_co
 import coiner_biz
-import robloxfunds_com
-import effective_fund_com
 
 def main():	
 	print "\n========== RUN get_stat.py ============"
 	util.logNow("START AT")
 		
-	webflipfunds_com.run()
 	custommining_com.run()
 	unitedswissbanks_com.run()
-	nanoproinvest_com.run()
-	bitcoinnetworks_biz.run()
-	city_finance_org.run()
-	greed_rush_com.run()
-	bitcoinpalas_com.run()
 	depdun_com.run()
-	paragoninvest_biz.run()
 	bullair_biz.run()	
 	oktrade_biz.run()
 	lucrativeventure_co.run()
 	coiner_biz.run()
-#	robloxfunds_com.run()
-#	effective_fund_com.run()
 	
 	util.logNow("END AT")
 
